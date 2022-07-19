815eb20a9ceaf05b5d9314f807a9274bcdf2073a
Carlos Lugo <calugo71@gmail.com>
2022-07-19 11:24:08 -0400

    commit tree 1156fca359cc5b23984a1e377ecf522a5bf4ce1f

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+divide(2000,100);
+const square = (parameter1) => parameter1 ** 2;
+square(5)
+const add = (parameter1, parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 + parameter2;
+  }
